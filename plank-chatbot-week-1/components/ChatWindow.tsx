ff6a611e67ba99ae0fbe44d1645357d75288b087
Artur H. de Oliveira <artur.oliveira@joinplank.com>
2025-03-13 16:09:57 -0300

    refactor: improve type definitions in langgraph and clean up unused imports in ChatWindow component

--- plank-chatbot-week-1/components/ChatWindow.tsx
+++ plank-chatbot-week-1/components/ChatWindow.tsx
@@ -8,11 +8,9 @@ import { toast } from "sonner";
 import { StickToBottom, useStickToBottomContext } from "use-stick-to-bottom";
 
 import { ChatMessageBubble } from "@/components/ChatMessageBubble";
-import { IntermediateStep } from "./IntermediateStep";
 import { Button } from "./ui/button";
 import { ArrowDown, LoaderCircle, Paperclip } from "lucide-react";
 import { Checkbox } from "./ui/checkbox";
-import { UploadDocumentsForm } from "./UploadDocumentsForm";
 import {
   Dialog,
   DialogContent,
@@ -33,9 +31,7 @@ function ChatMessages(props: {
   return (
     <div className="flex flex-col max-w-[768px] mx-auto pb-12 w-full">
       {props.messages.map((m, i) => {
-        if (m.role === "system") {
-          return <IntermediateStep key={m.id} message={m} />;
-        }
+
 
         const sourceKey = (props.messages.length - 1 - i).toString();
         return (
@@ -330,7 +326,6 @@ export function ChatWindow(props: {
                     Upload a document to use for the chat.
                   </DialogDescription>
                 </DialogHeader>
-                <UploadDocumentsForm />
               </DialogContent>
             </Dialog>
           )}
